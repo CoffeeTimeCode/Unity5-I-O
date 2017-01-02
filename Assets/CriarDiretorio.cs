55c9a11ec83ba33528d85e23077ce24703a3ed34
jsnunes <jesiel.nunes@hotmail.com>
2017-01-02 21:21:10 -0200

    Criando Diretório

---
+++ Assets/CriarDiretorio.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class CriarDiretorio : MonoBehaviour {
+	public string _local;
+	void Start () {
+		_local = Application.dataPath;
+		print (_local);
+		if (Directory.Exists (_local + "/teste(c#)")) {
+			print("Esse diretorio ja existe.");
+		} else {
+			Directory.CreateDirectory(_local + "/teste(c#)");
+		}
+	}
+	void Update () {}
+}
---
+++ Assets/CriarDiretorio.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+import System.IO;
+
+public var _local : String;
+
+function Start () {
+	_local = Application.dataPath;
+	print(_local);
+	if(Directory.Exists(_local+"teste(javascript)")){
+		print("Esse diretorio ja existe.");
+	}else{
+		Directory.CreateDirectory(_local + "/teste(javascript)");
+	}
+}
+function Update () {}
